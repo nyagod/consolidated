173ed6e1252c3849eeebf214800f7bdcc6d1b743
lolltenzin <your-email@example.com>
2024-11-24 16:22:20 +0545

    changes

--- apps/backend/api/undyed-yarn.js
+++ apps/backend/api/undyed-yarn.js
@@ -17,14 +17,14 @@ async function readExcelFile(filePath) {
 // Load the workbook and access the 'Consolidated' sheet
 async function loadData() {
     // Change the file path to point to the same directory as the API
-    const filePath = path.join(process.cwd(), './api/CONSOLIDATED REPORT OCTOBER 2024.xlsx');
+    const filePath = path.join(process.cwd(), 'CONSOLIDATED REPORT OCTOBER 2024.xlsx');
     const workbook = await readExcelFile(filePath);
     const sheet = workbook.Sheets['Consolidated'];
     
     // Limit the range by specifying rows and columns (for example, A1:F50)
     const range = 'A1:F400'; // Adjust this range based on your needs
     const rawData = xlsx.utils.sheet_to_json(sheet, { header: 1, defval: null, range: range });
-    
+    console.log(rawData);
     return rawData;
 }
 
